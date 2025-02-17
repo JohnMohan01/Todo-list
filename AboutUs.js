fb063bc6160ec7c7b6ff9f5ab748f4567dc486b4
John <61958240+JohnMohan01@users.noreply.github.com>
2025-02-17 17:21:02 +0530

    Add files via upload

---
+++ AboutUs.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const AboutUs = () => {
+    const [verticals, setverticals] = useState([
+      {image:'', title: 'Supply Chain Sustainability', body:'Coming up with the framework for tracking carbon footprint. Track, manage, and reduce carbon footprint in logistics sector.', id: 1 },
+      {image:'', title: 'Logistics Worker Wellness', body:'Focused toward driver wellness and safety. Developing the technology for tracking and measuring the driver behaviour and provide recommendation to improve wellness.', id: 2  },
+      {image:'', title: 'Algorithms and ML', body:'Developing algorithms, software systems and machine learning techniques for efficient logistics operations.', id: 3  },
+      {image:'', title: 'Logistics Infrastructure', body:'Technology development for the better logistics infrastructure. This would include integration of fuel efficient vehicle, drones in warehouse and last mile logistics etc.', id: 4  }
+    ]);
+    const handleClick = () => {
+        console.log('hello, ninjas');
+    }
+    return ( 
+        <div className="home">
+            {verticals.map((vertical) => (
+              <div className="verticals-preview" key={vertical.id}>
+                <h2>{ vertical.title }</h2>
+                </div>  
+            ))}
+        </div>
+     );
+}
+ 
+export default AboutUs;
---
+++ Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+        <h1>Logo</h1> 
+        <div className="links">
+          <a href ="/">Home</a>
+          <a href ="/About">About us</a>
+          <a href ="/People">People</a>
+          <a href ="/Research">Research</a>
+          <a href ="/Events">Events</a>
+          <a href ="/Careers">Careers</a>
+        </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
